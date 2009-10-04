79d9efb80d65f2d7a7d49750ded93dd02e4445a1
Jimmy Hedman <jimmy.hedman@southpole.se>
2009-10-04 21:48:50 +0200

    Added .bzr and made it work afte gitsvn
    - Added support for Bazaar repos.
    - If gitsvn has been used to import/convert a subversion repo
      it has both .svn and .git. Made it use git in that case.

--- vcupdate.sh
+++ vcupdate.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
+#  
+#
 
 if [ -d CVS/ ]; then
 	cvs update
-elif [ -d .svn/ ]; then
-	svn update
 elif [ -d .git/ ]; then
 	git pull
+elif [ -d .bzr/ ]; then
+	bzr upgrade
+elif [ -d .svn/ ]; then
+	svn update
 fi
