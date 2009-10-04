cae114cb20543b58831c9090d49b3ad8598fbd52
Jimmy Hedman <jimmy.hedman@southpole.se>
2009-10-04 22:08:51 +0200

    Check for binaries and added cvs flags.
    - Let it check if the applications are installed and warn if it's not
    - Added some CVS flags.

--- vcupdate.sh
+++ vcupdate.sh
@@ -3,14 +3,34 @@
 #
 
 if [ -d CVS/ ]; then
-	cvs update
+	if which cvs > /dev/null; then
+		cvs update -Pd
+	else
+		echo "You need CVS to update this directory"
+	fi
 elif [ -d .git/ ]; then
-	git pull
+	if which git > /dev/null; then
+		git pull
+	else
+		echo "You need GIT to update this directory"
+	fi
 elif [ -d .bzr/ ]; then
-	bzr pull
+	if which bzr > /dev/null; then
+		bzr pull
+	else
+		echo "You need Bazaar to update this directory"
+	fi
 elif [ -d .hg/ ]; then
-	hg pull
-	hg update
+	if which hg > /dev/null; then
+		hg pull
+		hg update
+	else
+		echo "You need Mercurial to update this directory"
+	fi
 elif [ -d .svn/ ]; then
-	svn update
+	if which svn > /dev/null; then
+		svn update
+	else
+		echo "You need Subversion to update this directory"
+	fi
 fi
